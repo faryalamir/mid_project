ea9483414043dc01f9359d1d19490c13261951a4
Faryal <faryalsipra@gmail.com>
2023-10-03 10:28:38 +0200

    Fix question 1

--- README.md
+++ README.md
@@ -1,34 +1,33 @@
 Python Questions:
+
 Python Question - 1:
+
 Write Python code to fetch historical health care R&D spending data from a given data source and calculate the year-over-year percentage change for each year over the past four years.
 Python Question - 2:
+
 Create a Python script that uses matplotlib library to generate a line chart or bar chart showing the trend in health care R&D spending for a specific company listed on the New York Stock Exchange over the past four years.
 SQL Questions:
 SQL Question - 3:
+
 write an SQL query to retrieve the total health care R&D spending for all companies for each of the past four years.
 SQL Question - 4:
-Write an SQL query to find the top three companies listed on the New York Stock Exchange with the highest percentage increase in health care R&D spending .
+
+Write an SQL query to find the top three companies listed on the New York Stock Exchange with the highest percentage increase 
+in health care R&D spending .
 Que 5:
 
 "What is the year-over-year percentage change in Health Care R&D spending for the past four years?"
 
 DATA ANALYSIS : 
 ------------------------
-The Box and Whisker Plot with outliers is representing the distribution of year over year spend on R&D in the health care sector.
-The mean number of spend over 4 years has a slight increase from 995 to 1159 million dollars.
-There is a clear distinction between some companies spending way more than average spend in the health care sector on R&D. This can be seen from the outliers and right skewness of box plot and an elevated range of approximately 8 billion
+Write Python code to fetch historical health care R&D spending data from a given data source and calculate the year-over-year percentage change for each year over the past four years.
 
 -------------------------
-
-5 of the 48 companies form the bulk spending of 70% of the health care sector on R&D.
-This is in range from 4 to 8 billion per year. This is quite elevated from the average of the sector at around 1 billion per year.
-14 out of 48 companies have no spending on R&D. This is approximately 30% of the health sector as seen on the next graph.
-The standard deviation of 1.9 billion is double of mean representing reasonably large variations in spend across companies considering outliers.
-
---------------------------------------------------------
 Filter health sector  > R&D 
 
---
+--------------------------------------------------------
+
+---------------------------------
 How health sector R&D are spending year over year 
 
 
--- main.py
+++ main.py
@@ -1,25 +1,84 @@
 import pandas as pd
 from pandas import DataFrame
-from os.path import abspath, dirname, join
+from os.path import dirname, join
+import matplotlib.pyplot as plt
 
-base_path = dirname(__file__)
-print(base_path)
-data_path = join(base_path, "data/raw/companies.xlsx")
-pd.set_option('display.max_columns', None)
+data_path = join(dirname(__file__), "data/raw/companies.xlsx")
+pd.set_option("display.max_columns", None)
 pd.set_option("display.max_rows", None)
 
-def normalize(df):
-    df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '').str.replace('.', '')
+
+def normalize_column_names(df: DataFrame) -> DataFrame:
+    df.columns = (
+        df.columns.str.strip()
+        .str.lower()
+        .str.replace(" ", "_")
+        .str.replace("(", "")
+        .str.replace(")", "")
+        .str.replace(".", "")
+    )
     return df
 
 
+def q1(df: DataFrame) -> None:
+    # Create a new dataframe with filtered columns
+    df_filtered = pd.DataFrame(
+        df,
+        columns=[
+            "ticker_symbol",
+            "years",
+            "period_ending",
+            "research_and_development",
+            "gics_sector",
+        ],
+    )
+    # Make sure we only have the health care sector companies
+    df_filtered = df_filtered.loc[df_filtered["gics_sector"] == "Health Care"]
+    # Sort by ticket symbol and years early on
+    df_filtered = df_filtered.sort_values(["ticker_symbol", "years"], ascending=True)
+    print(df_filtered.dtypes)
+    # Aggregated yearly spending change across the companies
+    yearly_spending = (
+        df_filtered.groupby("years")["research_and_development"].sum().reset_index()
+    )
+    # Calculate percentage change across years
+    yearly_spending["spending_change"] = yearly_spending[
+        "research_and_development"
+    ].pct_change()
+    # Fix NaN in the first year record as the pct_change needs a previous record to calculate change and there is none
+    yearly_spending = yearly_spending.fillna(0)
+    # Round off change
+    yearly_spending["spending_change"] = yearly_spending["spending_change"].round(4)
+    # Show records of the data
+    print(yearly_spending)
+    # Plotting YoY R&D spending change in the health sector
+    plt.figure(figsize=(10, 6))
+    plt.plot(
+        yearly_spending["years"],
+        yearly_spending["spending_change"],
+        marker="o",
+        linestyle="-",
+    )
+    plt.xlabel("Year")
+    plt.ylabel("Spending Change")
+    plt.grid(True)
+    plt.show()
+
+
 def main():
-    df = pd.read_excel(data_path)
-    df_cleaned = normalize(df)
-    df_filtered = pd.DataFrame(df_cleaned, columns=['ticker_symbol', 'years', 'period_ending', 'research_and_development', 'gics_sector'])
-    df_filtered = df_filtered[df_filtered['gics_sector'] == 'Health Care']
-    print(df_filtered)
-    print(df_filtered['period_ending'].drop_duplicates().sort_values())
+    # Load data
+    df = pd.read_excel(data_path, engine="openpyxl")
+    # Normalize data
+    df_cleaned = normalize_column_names(df)
+    # Clean years
+    df_cleaned["years"] = df_cleaned["years"].replace("Year ", "", regex=True)
+    df_cleaned["years"] = df_cleaned["years"].astype("int")
+
+    print(df_cleaned.head())
+    print(df_cleaned.dtypes)
+
+    # Question 1
+    q1(df_cleaned)
 
 
 if __name__ == "__main__":
